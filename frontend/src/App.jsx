11586cf0c08d078bbced46c3b3e4234d7f6350b8
Muhammad Amsal <muhammadamsal526@gmail.com>
2024-10-06 23:45:37 +0500

    navbar com

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main className="bg-primary text-tertiary">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mens" element={<Category />} />
+          <Route path="/womens" element={<Category />} />
+          <Route path="/kids" element={<Category />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart-page" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,48 @@
+import {useState,React} from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import logo from '../assets/logo.svg'
+import logout from '../assets/logout.svg'
+import Navbar from './Navbar'
+import { MdClose } from 'react-icons/md'
+import { MdMenu } from 'react-icons/md'
+import { FaOpencart } from 'react-icons/fa'
+
+
+export default function Header() {
+const [menuOpened, setmenuOpened] = useState(false)
+const toggleMenu=()=> setmenuOpened(!menuOpened)
+
+  return (
+    <header className='fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10' >
+     <div className='px-4 flexBetween py-3 max-xs:px-2' >
+      {/* logo */}
+      <div>
+       <Link><img src={logo} height={66} width={88} /></Link>
+      </div>
+      {/* Navbar desktop */}
+      <Navbar containerStyles={'hidden md:flex gap-x-5 xl:gap-x-10 medium-15'} />
+      {/* navbar mobiles */}
+      <Navbar containerStyles={`${menuOpened ? "flex item-start flex-col gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300"
+       : "flex item-start flex-col gap-y-12 fixed top-20 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 -right-[100%]"}`} />
+      {/* buttons */}
+      <div className='flexBetween sm:gap-x-2 bold-16' >
+        {!menuOpened? (
+          <MdMenu onClick={toggleMenu} className='md:hidden hover:ring-secondary cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full ' />
+
+          ):(
+
+          <MdClose onClick={toggleMenu} className='md:hidden hover:ring-secondary cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full ' />
+          )}
+          <div className='flexBetween sm:gap-x-6' >
+              <NavLink to={"cart-page"} className={'flex'} ><FaOpencart className='p-1 w-8 h-8 ring-slate-900/30 ring-1 rounded-full' />
+                <span className='relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2' >0</span>              
+              </NavLink>
+              <NavLink to={'logout'} className={'btn_secondary_rounded flexCenter gap-x-2 medium-16'} ><img alt='logoutIcon' height={19} width={19} src={logout} />logout</NavLink>
+          </div>
+      </div>
+
+    </div>
+   </header>
+    
+  )
+}
